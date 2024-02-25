4da3feeca22bc0747d02bedd71583d95b6f5caca
Chaitanya Tripathi <nithral@protonmail.com>
2024-02-25 16:00:36 +0530

    Prisma Set up

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,9 +1,14 @@
-import Link from "next/link";
-import React from "react";
+"use client";
 
 import { AiFillBug } from "react-icons/ai";
 
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import classNames from "classnames";
+
 const NavBar = () => {
+  const currentPath = usePathname();
+
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
@@ -18,7 +23,11 @@ const NavBar = () => {
           <Link
             href={link.href}
             key={link.href}
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            className={classNames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
           >
             {link.label}
           </Link>
