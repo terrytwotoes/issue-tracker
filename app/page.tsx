800b248b85fbccee7569fe57b5a1f3dd8df9e105
nithral <nithral@protonmail.com>
2024-04-08 13:10:10 +0530

    Improved Layout of the dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import prisma from "@/prisma/client";
 import IssueSummary from "./IssueSummary";
 import LatestIssues from "./LatestIssues";
 import IssueChart from "./IssueChart";
+import { Flex, Grid } from "@radix-ui/themes";
 
 export default async function Home() {
   const open = await prisma.issue.count({
@@ -14,6 +15,15 @@ export default async function Home() {
     where: { status: "CLOSED" },
   });
 
-  // return <LatestIssues />;
-  return <IssueChart open={open} inProgress={inProgress} closed={closed} />;
+  const issueProps = { open, inProgress, closed };
+
+  return (
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Flex direction="column" gap="5">
+        <IssueSummary {...issueProps} />
+        <IssueChart {...issueProps} />
+      </Flex>
+      <LatestIssues />
+    </Grid>
+  );
 }
