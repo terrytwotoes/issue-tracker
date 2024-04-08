8be5afcf7b3b2e5db756bd2f668c52550e560755
nithral <nithral@protonmail.com>
2024-04-08 13:21:41 +0530

    Added Metadata for better SEO

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -4,6 +4,7 @@ import { Status } from "@prisma/client";
 import IssueActions from "./IssueActions";
 import IssueTable, { IssueQuery, columnNames } from "./IssueTable";
 import { Flex } from "@radix-ui/themes";
+import { Metadata } from "next";
 
 interface Props {
   searchParams: IssueQuery;
@@ -50,4 +51,10 @@ const IssuesPage = async ({ searchParams }: Props) => {
 
 //Telling NextJS to opt out of static rendering
 export const dynamic = "force-dynamic";
+
+export const metadata: Metadata = {
+  title: "Issue Tracker - Issue List",
+  description: "View all project issues",
+};
+
 export default IssuesPage;
